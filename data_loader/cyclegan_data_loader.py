98b902ebeb6e2cb2bca248d0084eeea85f54ab2e
Holly Grimm <holly.grimm@gmail.com>
2019-07-22 15:25:29 -0600

    document code

--- data_loader/cyclegan_data_loader.py
+++ data_loader/cyclegan_data_loader.py
@@ -3,24 +3,30 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from base.base_data_loader import BaseDataLoader
 
+
 class CycleGANDataLoader(BaseDataLoader):
 
-
     def __init__(self, config):
+        """Initialize dataset
+        Args:
+            config : dictionary
+        """
         super(CycleGANDataLoader, self).__init__(config)
         self.BUFFER_SIZE = config['buffer_size']
         self.BATCH_SIZE = config['batch_size']
         self.IMG_HEIGHT = config['img_height']
         self.IMG_WIDTH = config['img_width']
-        self.AUTOTUNE = tf.data.experimental.AUTOTUNE        
+
+        # delegate the setting of num_parallel_calls to tf.data runtime
+        self.AUTOTUNE = tf.data.experimental.AUTOTUNE
 
         dataset, metadata = tfds.load('cycle_gan/horse2zebra',
-                                    with_info=True, as_supervised=True)
+                                      with_info=True, as_supervised=True)
 
         self.train_a, self.train_b = dataset['trainA'], dataset['trainB']
         self.test_a, self.test_b = dataset['testA'], dataset['testB']
-        
 
+        # transform the data, cache, shuffle, and batch
         self.train_a = self.train_a.map(
             self.preprocess_image_train, num_parallel_calls=self.AUTOTUNE).cache().shuffle(
             self.BUFFER_SIZE).batch(self.BATCH_SIZE)
@@ -37,40 +43,61 @@ class CycleGANDataLoader(BaseDataLoader):
             self.preprocess_image_test, num_parallel_calls=self.AUTOTUNE).cache().shuffle(
             self.BUFFER_SIZE).batch(self.BATCH_SIZE)
 
-
     def random_crop(self, image):
+        """Crop the image randomly to a given size
+        Args:
+            image : tensor
+        Returns:
+            cropped image (tensor)
+        """
         cropped_image = tf.image.random_crop(
-        image, size=[self.IMG_HEIGHT, self.IMG_WIDTH, 3])
-
+            image, size=[self.IMG_HEIGHT, self.IMG_WIDTH, 3])
         return cropped_image
 
-
-    # normalizing the images to [-1, 1]
     def normalize(self, image):
+        """Normalize the image data to [-1, 1]
+        Args:
+            image : tensor
+        Returns:
+            normalized image (tensor)
+        """
         image = tf.cast(image, tf.float32)
         image = (image / 127.5) - 1
         return image
 
-
     def random_jitter(self, image):
-        # resizing to 286 x 286 x 3
-        image = tf.image.resize(image, [286, 286],
+        """Increase height and width by 30 pixels, random crop back to orig size, and randomly flip left to right
+        Args:
+            image : tensor
+        Returns:
+            image w/random jitter (tensor)
+        """
+        SIZE_INCREASE = 30
+        image = tf.image.resize(image, [self.IMG_HEIGHT + SIZE_INCREASE, self.IMG_WIDTH + SIZE_INCREASE],
                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-        # randomly cropping to 256 x 256 x 3
+        # randomly crop back to 256 x 256 x 3
         image = self.random_crop(image)
-
-        # random mirroring
         image = tf.image.random_flip_left_right(image)
-
         return image
 
     def preprocess_image_train(self, image, label):
+        """Apply random jitter, then normalize image
+        Args:
+            image : tensor
+        Returns:
+            image w/random jitter and normalization (tensor)
+        """
         image = self.random_jitter(image)
         image = self.normalize(image)
         return image
 
     def preprocess_image_test(self, image, label):
+        """Normalize image
+        Args:
+            image : tensor
+        Returns:
+            normalized image (tensor)
+        """
         image = self.normalize(image)
         return image
 
@@ -78,19 +105,10 @@ class CycleGANDataLoader(BaseDataLoader):
         return self.train_a
 
     def get_train_b_data(self):
-        return self.train_b   
+        return self.train_b
 
     def get_test_a_data(self):
         return self.test_a
 
     def get_test_b_data(self):
-        return self.test_b     
-
-
-
-
-
-
-
-
-    
+        return self.test_b
